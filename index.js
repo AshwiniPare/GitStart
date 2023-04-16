ecda988e7b5a55dce52bfa60faf54d709f056377
AshwiniPare <ashwinipbhatt@gmail.com>
2023-04-16 16:58:42 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+console.dir(document);
+
+var header = document.getElementById('main-header');
+header.style.borderBottom = "solid 3px #000"
+
+var headerTitle = document.getElementById('addItems');
+headerTitle.style.font = "bold";
+headerTitle.style.color = "green";
