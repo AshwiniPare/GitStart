6c655806acb154de08170e0f131a0b6b52c4c532
AshwiniPare <ashwinipbhatt@gmail.com>
2023-04-17 06:40:54 -0700

    Adding changes for getElementByTagName

--- index.js
+++ index.js
@@ -15,3 +15,15 @@ for(var i=0;i<items.length;i++)
 {
     items[i].style.fontWeight = 'bold';
 }
+
+var li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent = "Hello2";
+li[1].style.fontWeight = 'bold';
+li[1].style.backgroundColor = 'yellow';
+
+for(var i=0;i<li.length;i++)
+{
+    li[i].style.backgroundColor = 'red';
+}
