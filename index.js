139d157d907a6af3733c7d501c2ca28a1fbc9861
AshwiniPare <ashwinipbhatt@gmail.com>
2023-04-16 17:45:48 -0700

    Adding getElementsByClassName

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ header.style.borderBottom = "solid 3px #000"
 var headerTitle = document.getElementById('addItems');
 headerTitle.style.font = "bold";
 headerTitle.style.color = "green";
+
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+items[2].style.backgroundColor = 'green';
+
+for(var i=0;i<items.length;i++)
+{
+    items[i].style.fontWeight = 'bold';
+}
