978d7d4fb0016204b9c1b10bdbdac58c8a4fd2bd
AshwiniPare <ashwinipbhatt@gmail.com>
2023-04-17 18:08:20 -0700

    Storing users in local storage in right format

--- BookAppointment.js
+++ BookAppointment.js
@@ -1,14 +1,20 @@
 function submitForm()
-        {
-            var input = document.getElementById('name');
-            //console.log(input.value);
-            localStorage.setItem('userName','ashi');
-            var input = document.getElementById('email');
-            console.log(input.value);
-            var input = document.getElementById('phone');
-            console.log(input.value);
-            var input = document.getElementById('date');
-            console.log(input.value);
-            var input = document.getElementById('time');
-            console.log(input.value);
-        }
+{
+    
+    var input = document.getElementById('name');
+    let myObj = {userName: input.value};
+    var input = document.getElementById('email');
+    myObj.userEmail = input.value;
+    var input = document.getElementById('phone');
+    myObj.userPhone = input.value;
+    var input = document.getElementById('date');
+    myObj.userDate = input.value;
+    var input = document.getElementById('time');
+    myObj.userTime = input.value;
+
+    let myObjSerialized = JSON.stringify(myObj);
+    localStorage.setItem("myObj", myObjSerialized);
+
+    let myObjDeserialized=JSON.parse(localStorage.getItem("myObj"));
+    console.log(myObjDeserialized);
+}
