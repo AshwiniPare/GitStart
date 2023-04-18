663362b803b967e3fd31e7df7ea7478035106ac8
AshwiniPare <ashwinipbhatt@gmail.com>
2023-04-17 17:21:02 -0700

    BookAppointmentApp

---
+++ BookAppointment.js
@@ -0,0 +1,14 @@
+function submitForm()
+        {
+            var input = document.getElementById('name');
+            //console.log(input.value);
+            localStorage.setItem('userName','ashi');
+            var input = document.getElementById('email');
+            console.log(input.value);
+            var input = document.getElementById('phone');
+            console.log(input.value);
+            var input = document.getElementById('date');
+            console.log(input.value);
+            var input = document.getElementById('time');
+            console.log(input.value);
+        }
