19f38a5b5ea58e56fa9f4a71e5019901581aec40
AshwiniPare <ashwinipbhatt@gmail.com>
2023-05-02 11:12:36 -0700

    promiseAll.js

---
+++ promiseAll.js
@@ -0,0 +1,54 @@
+const user = {
+    name : "Ashwini",
+    lastActivityTime : '13th of Jan'
+}
+
+const posts = [];
+
+function updateLastUserActivityTime() {
+    return new Promise( (resolve, reject) => {
+        user.lastActivityTime = new Date().getTime();
+        resolve(user.lastActivityTime);
+    })
+}
+
+function createPost(post) {
+    return new Promise( (resolve, reject) => {
+        setTimeout( () => {
+            posts.push(post);
+            resolve(post);
+        }, 1000);
+    })
+}
+
+function deletePost() {
+    return new Promise( (resolve, reject) => {
+        setTimeout( () => {
+            if(posts.length > 0) {
+                const poppedElem = posts.pop();
+                resolve(poppedElem);
+            }
+            else {
+                reject("ERROR: ARRAY IS EMPTY");
+            }
+        },1000);
+    })
+}
+
+Promise.all([createPost({title: 'POST1'}), updateLastUserActivityTime()])
+    .then (values => console.log(values))
+    .catch(err => console.log(err))
+
+Promise.all([createPost({title: 'POST2'}), updateLastUserActivityTime()])
+    .then (values => {
+        console.log(values);
+        Promise.all([deletePost(), updateLastUserActivityTime()])
+       .then( console.log(posts))
+        .catch(err => console.log(err));
+    })
+    .catch(err => console.log(err))
+
+
+
+
+    
